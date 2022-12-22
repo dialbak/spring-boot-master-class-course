1da04ad91a147c40c2e98633bbee3ca491f1cd8f
Diallo Bakary <bakary.diallo.simplon@gmail.com>
2022-12-22 12:20:01 +0100

    CustomerController

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,7 +2,6 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -13,18 +12,4 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
     }
 
-    @GetMapping
-    Customer getCustomer() {
-        return new Customer(1L, "James bond");
-    }
-
-    class Customer {
-        private final Long id;
-        private final String name;
-
-        public Customer(Long id, String name) {
-            this.id = id;
-            this.name = name;
-        }
-    }
 }
