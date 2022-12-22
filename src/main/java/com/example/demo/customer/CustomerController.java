804129017f3d70ddfb0764cb0ae0b8c5a1dc57d2
Diallo Bakary <bakary.diallo.simplon@gmail.com>
2022-12-22 16:00:24 +0100

    dependency injection, switch implementation

--- src/main/java/com/example/demo/customer/CustomerController.java
+++ src/main/java/com/example/demo/customer/CustomerController.java
@@ -3,11 +3,20 @@ package com.example.demo.customer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class CustomerController {
 
-    @GetMapping
-    Customer getCustomer() {
-        return new Customer(1L, "Bk");
+    private CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
     }
+
+    @GetMapping
+    List<Customer> getCustomer() {
+        return customerService.getCustomer();
+    }
+
 }
--- src/main/java/com/example/demo/customer/CustomerService.java
+++ src/main/java/com/example/demo/customer/CustomerService.java
@@ -1,4 +1,21 @@
 package com.example.demo.customer;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CustomerService {
+
+    private final CustomerRepo customerRepo;
+
+    // public CustomerService(@Qualifier("fake") CustomerRepo customerRepo) {
+    public CustomerService(CustomerRepo customerRepo) {
+        this.customerRepo = customerRepo;
+    }
+
+
+    List<Customer> getCustomer() {
+        return customerRepo.getCustomers();
+    }
 }
